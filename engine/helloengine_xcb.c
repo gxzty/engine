d1e70ed82f6a970d64de2541b64886fc8f14427d
Zty <361222163@qq.com>
2017-08-30 14:19:59 +0800

    black rect

--- engine/helloengine_xcb.c
+++ engine/helloengine_xcb.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <xcb/xcb.h>
-
-int main(void) {
-    xcb_connection_t *pConn;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//#include <xcb/xcb.h>
+//
+//int main(void) {
+//    xcb_connection_t *pConn;
+//}
